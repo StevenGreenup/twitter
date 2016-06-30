e221f53d9e08f2022398ada7f96c9bc6fb3ed61f
StevenGreenup <greenup.steven@gmail.com>
2016-06-30 15:43:57 -0500

    added profiles for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
   def new
     @user = User.new
   end
@@ -7,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new
     @user.username = params[:user][:username]
     @user.password = params[:user][:password]
+    @user.photo_url = params[:user][:photo_url]
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
       redirect_to root_path, notice: "Welcome #{@user.username}"
