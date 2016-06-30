f997200a2fb7a1b43329de9d1b96fc2c6f6a0801
StevenGreenup <greenup.steven@gmail.com>
2016-06-30 15:05:21 -0500

    added feature sign in after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
 get "registration" => "users#new",  as: :new_user
 post "registration" => "users#create", as: :create_user
+
+get "sign_in" => "sessions#new", as: :sign_in
+post "sign_in" => "sessions#create"
+delete "sign_in" => "sessions#delete"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
