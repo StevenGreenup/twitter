d973753d7b4fa897eab2d302b57af8507a9011af
StevenGreenup <greenup.steven@gmail.com>
2016-07-03 08:44:13 -0500

    added alert messages for improper data entry on new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  validates :username, presence: true
+  validates :photo_url, presence: true
+
   has_many :tweets
   acts_as_follower
   acts_as_followable
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'faker'
+#
+#
+# User.create username:,photo_url:,
+#
+#   50.times do |i|
+#     file << [
+#       i+1,
+#       Faker::Commerce.product_name,
+#       Faker::Hipster.paragraph(2, false, 4),
+#       Faker::Avatar.image(i+1, "500x500", "png"),
+#       Faker::Commerce.price,
+#       Faker::Commerce.department
+#     ]
+#
+#   end
+#
+#
+# end
