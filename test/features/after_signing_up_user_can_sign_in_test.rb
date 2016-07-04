fa825bb9caa463ceba273bf1e8ae28079ed59b71
StevenGreenup <greenup.steven@gmail.com>
2016-07-04 14:10:50 -0500

    added a test to sign in, sign out, and sign up

--- test/features/after_signing_up_user_can_sign_in_test.rb
+++ test/features/after_signing_up_user_can_sign_in_test.rb
@@ -3,20 +3,17 @@ require "test_helper"
 class AfterSigningUpUserCanSignInTest < Capybara::Rails::TestCase
 
   test "Users can sign in after signing up" do
-    skip()
-    visit root_path
-
-    click_link "Sign up"
+    visit new_user_path
     fill_in "Username", with: "testing"
     fill_in "Photo url", with: "https://pbs.twimg.com/profile_images/727184392859185153/rLNPyViG.jpg"
     fill_in "Password", with: "testing"
     fill_in "Password confirmation", with: "testing"
     click_button "Create User"
-
-    click_link "Sign in"
+    assert_content "testing"
+    click_link "Sign Out"
     fill_in "Username", with: "testing"
     fill_in "Password", with: "testing"
     click_button "Sign in"
-    assert_content "Welcome back, testing!"
+    assert_content "testing"
   end
 end
