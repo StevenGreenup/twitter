2038eb8e5ca2ab521ba83f40b2836296edd20e74
StevenGreenup <greenup.steven@gmail.com>
2016-07-04 11:51:43 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require 'faker'
-
+User.delete_all
+Tweet.Delete_all
 
 100.times do |i|
   User.create username: "#{Faker::Internet.user_name}", photo_url: "#{Faker::Avatar.image(i+1, "300x300")}", password: "test"
+end
+
+100.times do |i|
+  Tweet.create user_id: "#{i+1}", body: "#{Faker::Hipster.paragraph(i+1)}"
 end
